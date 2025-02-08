a06695a762163b70e4c576e97770cced2c58ca07
JonathanK87 <jkang87@uic.edu>
2025-02-08 11:32:21 -0600

    backend database creation fixing

--- backend/main.py
+++ backend/main.py
@@ -53,64 +53,13 @@ def main():
                 vegetarian TINYINT
             )
             """
-            parkQuery = """
-            CREATE TABLE parks(
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                latitude FLOAT,
-                longitude FLOAT,
-                name VARCHAR(255),
-                address VARCHAR(255),
-                rating INT,
-                description VARCHAR(255),
-                vibe VARCHAR(255)
-            )
-            """
-            museumQuery = """
-            CREATE TABLE museums(
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                latitude FLOAT,
-                longitude FLOAT,
-                name VARCHAR(255),
-                address VARCHAR(255),
-                rating INT,
-                price INT,
-                description VARCHAR(255),
-                vibe VARCHAR(255)
-            )
-            """
-            karaokeQuery = """
-            CREATE TABLE karaoke(
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                latitude FLOAT,
-                longitude FLOAT,
-                name VARCHAR(255),
-                address VARCHAR(255),
-                rating INT,
-                price INT,
-                description VARCHAR(255),
-                vibe VARCHAR(255)
-            )
-            """
-            thriftingQuery = """
-            CREATE TABLE thrifting(
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                latitude FLOAT,
-                longitude FLOAT,
-                name VARCHAR(255),
-                address VARCHAR(255),
-                rating INT,
-                price INT,
-                description VARCHAR(255),
-                products VARCHAR(255),
-                vibe VARCHAR(255)
-            )
-            """
-            cursor.execute(restaurantQuery)
-            cursor.execute(parkQuery)
-            cursor.execute(museumQuery)
-            cursor.execute(karaokeQuery)
-            cursor.execute(thriftingQuery)
-            connection.commit()
+            cursor.execute("SHOW TABLES LIKE 'restaurants'")
+            result = cursor.fetchone()
+            if result:
+                print("Table exists")
+            else:
+                cursor.execute(restaurantQuery)
+                connection.commit()
             cursor.execute("SHOW TABLES") #to see the tables, checking
     except Error as e:
         print(e)
