a48aecbfdcd8846a803fc74abbea2f0deb7cefdd
JonathanK87 <jkang87@uic.edu>
2025-02-08 02:29:51 -0600

    hello this is to add a bit of mysql code

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,27 @@
+from getpass import getpass
+from mysql.connector import connect, Error
+
+
+
+def main():
+    try:
+        with connect(
+            host="localhost",
+            user=input("Enter username: "),
+            password=getpass("Enter password: "),
+        ) as connection:
+            cursor = connection.cursor()
+            cursor.execute("SHOW DATABASES LIKE 'establishments'")
+            result = cursor.fetchone()
+            if result:
+                print("Database exists") # placeholder
+            else:
+                cursor.execute("CREATE DATABASE establishments")
+                print("created database")
+            connection.database = "establishments"
+            print("connected")
+    except Error as e:
+        print(e)
+
 if __name__ == '__main__':
-    print('hello')
+    main()
